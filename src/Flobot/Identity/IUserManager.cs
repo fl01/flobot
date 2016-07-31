99e0e40ec54538f8f53c30ca38e4aa39525127d8
Artem <artemjjke@gmail.com>
2016-07-31 18:56:50 +0300

    added usermanger

---
+++ src/Flobot/Identity/IUserManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Connector;
+
+namespace Flobot.Identity
+{
+    public interface IUserManager
+    {
+        User RecognizeUser(ChannelAccount account);
+    }
+}
