a38e0f50851884d10a93fb80faf166999245f360
Artem <artemjjke@gmail.com>
2016-08-15 07:20:41 +0300

    added SettingsService

---
+++ src/Flobot/Settings/ConfigSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Flobot.Logging;
+
+namespace Flobot.Settings
+{
+    public class ConfigSettings : IConfigSettings
+    {
+        private readonly ILog logger;
+
+        public ConfigSettings(ILoggingService loggingService)
+        {
+            this.logger = loggingService.GetLogger(this);
+        }
+
+        public string GetCommandPrefix()
+        {
+            return GetAppKeyValue("CommandPrefix");
+        }
+
+        public string GetSubCommandSeparator()
+        {
+            return GetAppKeyValue("SubCommandSeparator");
+        }
+
+        private string GetAppKeyValue(string appkey)
+        {
+            try
+            {
+                return ConfigurationManager.AppSettings[appkey];
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                logger.Error(ex);
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ src/Flobot/Settings/SettingsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Flobot.Logging;
+
+namespace Flobot.Settings
+{
+    public class SettingsService : ISettingsService
+    {
+        private IConfigSettings configSettings;
+        private readonly ILog logger;
+
+        public SettingsService(ILoggingService loggingService, IConfigSettings configSettings)
+        {
+            this.logger = loggingService.GetLogger(this);
+            this.configSettings = configSettings;
+        }
+
+        public string GetCommandPrefix()
+        {
+            return configSettings.GetCommandPrefix();
+        }
+
+        public string GetSubCommandSeparator()
+        {
+            return configSettings.GetSubCommandSeparator();
+        }
+    }
+}
