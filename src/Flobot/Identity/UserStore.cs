5ade42ca816625b4f509f74d50a1b3806ebb08c9
Artem <artemjjke@gmail.com>
2016-08-15 14:45:57 +0300

    removed admin perm from test acc

--- src/Flobot/Identity/UserStore.cs
+++ src/Flobot/Identity/UserStore.cs
@@ -15,7 +15,7 @@ namespace Flobot.Identity
             {
                 new User() { Id = "29:1bg6E1DleuImOkBTeV71vPluAqvkdHJNxPidOccIXTW8", Name = "Artem V", Group = Group.Administrators, Role = Role.Admin },
                 new User() { Id = "29:1ANMOkUOM9d4f8-DgoQZeq6nbIW5MbJ0-WqjwvNY5DLM", Name = "psycho", Group = Group.Default | Group.PsychoRaid, Role = Role.User },
-                new User() { Id = "2c1c7fa3", Name = "User1", Group = Group.Administrators, Role = Role.Admin } // test acc used by bot channel emulator
+                new User() { Id = "2c1c7fa3", Name = "User1", Group = Group.Default | Group.PsychoRaid, Role = Role.Admin } // test acc used by bot channel emulator
             };
         }
 
