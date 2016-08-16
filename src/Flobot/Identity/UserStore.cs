75e6e27cf92ee425151b20f28982ba6cde07caea
Artem <artemjjke@gmail.com>
2016-08-17 00:40:57 +0300

    register user

--- src/Flobot/Identity/UserStore.cs
+++ src/Flobot/Identity/UserStore.cs
@@ -15,6 +15,7 @@ namespace Flobot.Identity
             {
                 new User() { Id = "29:1bg6E1DleuImOkBTeV71vPluAqvkdHJNxPidOccIXTW8", Name = "Artem V", Group = Group.Administrators, Role = Role.Admin },
                 new User() { Id = "29:1ANMOkUOM9d4f8-DgoQZeq6nbIW5MbJ0-WqjwvNY5DLM", Name = "psycho", Group = Group.Default | Group.PsychoRaid, Role = Role.User },
+                new User() { Id = "29:1D56unUb707tZy_nkhJ5GbWYC4enACN4ZhwwdglmhY7k", Name = "Aleksey P", Group = Group.Default | Group.PictureStoreCM, Role = Role.User },
                 new User() { Id = "2c1c7fa3", Name = "User1", Group = Group.Default | Group.PictureStoreCM, Role = Role.Admin } // test acc used by bot channel emulator
             };
         }
